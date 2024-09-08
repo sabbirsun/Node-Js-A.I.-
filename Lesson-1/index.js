cf70266e0369207f0e7d4252e2d6bcc93bea252e
sabbirsun <sabbirsun1920@gmail.com>
2024-09-09 02:37:47 +0600

    first commit

---
+++ Lesson-1/index.js
@@ -0,0 +1,4 @@
+const s1 =require('./student');
+console.log(s1.getName());
+console.log(s1.getAge());
+console.log(s1.cgpa);
---
+++ Lesson-1/student.js
@@ -0,0 +1,19 @@
+const getName = () =>{
+    return "Sabbirul Islam Sun"
+}
+
+const getAge = () =>{
+    return "24";
+}
+
+const cgpa = 3.92;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+module.exports = {
+    getName,
+    getAge,
+    cgpa
+}
