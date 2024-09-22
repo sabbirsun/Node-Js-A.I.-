14b1858ffac64a7b6e3cd2357c97e1b38a9cfc83
sabbirsun <sabbirsun1920@gmail.com>
2024-09-22 21:26:43 +0600

    header

--- Express Lesson-4/app.js
+++ Express Lesson-4/app.js
@@ -12,9 +12,14 @@ app.use("/register",(req, res)=>{
     res.sendFile(__dirname+"/views/register.html");
 });
 
-app.use("/login",(req, res)=>{
-    res.send("Hi, I'm a login page")
+app.get("/login", (req, res) => {
+    res.cookie("name", "Sabbir"); // Wrap the name in quotes
+    res.cookie("age", "30");
+    res.clearCookie("name");
+    res.append("id", "109");
+    res.end(); // End the response after setting cookies
 });
+
 app.use('/', (req, res) => {
     res.statusCode = 200;
     res.sendFile(__dirname+"/views/index.html");
