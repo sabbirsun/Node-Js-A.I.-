7b6119abbc0e4d923dad4d8b937d4d2b9b9b14bd
sabbirsun <sabbirsun1920@gmail.com>
2024-09-22 21:12:13 +0600

    statuscode

--- Express Lesson-4/app.js
+++ Express Lesson-4/app.js
@@ -5,14 +5,25 @@ app.use("/api/user",userRouter);
 
 // Define some routes (optional, for testing purposes)
 
-app.use('/', (req, res) => {
-    res.send('Hello, world!');
-    res.end();
+
+
+app.use("/register",(req, res)=>{
+    res.statusCode = 200;
+    res.sendFile(__dirname+"/views/register.html");
 });
+
+app.use("/login",(req, res)=>{
+    res.send("Hi, I'm a login page")
+});
+app.use('/', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(__dirname+"/views/index.html");
+});
+
 
 app.use((req, res)=>{
     res.send("<h1>404 !!! Not a valid url</h1>")
-})
+});
 
 // app.post('/', (req, res) => {
 //     res.send('Hello, world! Post');
