d0cddf5afb4930b5847e86ba3e941277266c6536
sabbirsun <sabbirsun1920@gmail.com>
2024-10-01 18:37:36 +0600

    commit

--- E Lesson-5/index.js
+++ E Lesson-5/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res)=>{
-    const {id , name} = req.query;
-    res.send(`Studen id is  : ${id}, Student name is  : ${name}`);
+app.get("/userID/:is/userAge/:age", (req, res)=>{
+   const id = req.params.id;
+   const age = req.params.age;
+
+    // const id = req.query.id;
+    // const name = req.query.name;
+    // const {id , name} = req.query;
+    res.send(`Studen id is  : ${id}, Student name is  : ${age}`);
 });
 
 app.listen(PORT, () => {
